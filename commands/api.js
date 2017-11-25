b3d2f9cb611db8a8bb075bf832ae1ec18ef9788c
psychonaut <diogotimote@gmail.com>
2017-11-25 12:36:25 -0200

    Save API to db

--- commands/api.js
+++ commands/api.js
@@ -1,12 +1,14 @@
 const Discord = require("discord.js");
+const database = require('./../lib/core/database');
 var waky = {};
 
-waky.run = function(command, client, message, server, language) {
+waky.run = function(command, client, message, server, sync) {
     if (!message.member.hasPermission('ADMINISTRATOR')) return;
     if (command.length !== 4) return message.reply("use: api apiname(divinepride) newapivalue");
     if (command[2] === 'divinepride') {
         server.divinepride = command[3];
         console.log(server.divinepride);
+        database.updateServer("divinepride", command[3], server.guildid);
         message.channel.send("Divine-pride api changed!");
         return;
     }
